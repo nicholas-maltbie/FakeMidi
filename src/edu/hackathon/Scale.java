26e13de74bec7af97b85211600d867631404a2dd
vuongkhuat <vuongkhuat0407@gmail.com>
2016-10-08 20:29:01 -0400

    update scale

---
+++ src/edu/hackathon/Scale.java
@@ -0,0 +1,51 @@
+package edu.hackathon;
+
+public class Scale {
+	public enum Type {
+		MAJOR, NATURAL_MINOR, HARMONIC_MINOR, MELODIC_MINOR
+	}
+	private Note[] notes;
+	private Type type;
+	public Scale(Note note, Type type) {
+		this.type = type;
+		notes = new Note[8];
+		notes[0] = note;
+		if (type == Type.MAJOR) {
+			notes[1] = notes[0].getWholeStepUp();
+			notes[2] = notes[1].getWholeStepUp();
+			notes[3] = notes[2].getHalfStepUp();
+			notes[4] = notes[3].getWholeStepUp();
+			notes[5] = notes[4].getWholeStepUp();
+			notes[6] = notes[5].getWholeStepUp();
+			notes[7] = notes[6].getHalfStepUp();
+		}
+		if (type == Type.NATURAL_MINOR) {
+			notes[1] = notes[0].getWholeStepUp();
+			notes[2] = notes[1].getHalfStepUp();
+			notes[3] = notes[2].getWholeStepUp();
+			notes[4] = notes[3].getWholeStepUp();
+			notes[5] = notes[4].getHalfStepUp();
+			notes[6] = notes[5].getWholeStepUp();
+			notes[7] = notes[6].getWholeStepUp();
+		}
+		if (type == Type.HARMONIC_MINOR) {
+			notes[1] = notes[0].getWholeStepUp();
+			notes[2] = notes[1].getHalfStepUp();
+			notes[3] = notes[2].getWholeStepUp();
+			notes[4] = notes[3].getWholeStepUp();
+			notes[5] = notes[4].getHalfStepUp();
+			Note temp = notes[5].getWholeStepUp();
+			notes[6] = temp.getHalfStepUp();
+			notes[7] = notes[6].getHalfStepUp();
+		}
+		if (type == Type.MELODIC_MINOR) {
+			notes[1] = notes[0].getWholeStepUp();
+			notes[2] = notes[1].getHalfStepUp();
+			notes[3] = notes[2].getWholeStepUp();
+			notes[4] = notes[3].getWholeStepUp();
+			notes[5] = notes[4].getWholeStepUp();
+			notes[6] = notes[5].getWholeStepUp();
+			notes[7] = notes[6].getHalfStepUp();
+		}
+	}
+}
